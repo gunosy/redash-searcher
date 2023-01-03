25258111d59af5c2ba7884af307891a324b34e87
Hironori Yamamoto <hironori.yamamoto@gunosy.com>
2023-01-03 11:50:56 +0900

    fix

---
+++ redash-searcher-web/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is workaround to set public environment variables from runtime
+# ref: https://dev.to/itsrennyman/manage-nextpublic-environment-variables-at-runtime-with-docker-53dl
+
+echo "Check that we have NEXT_PUBLIC_REDASH__URL vars"
+test -n "$NEXT_PUBLIC_REDASH__URL"
+
+find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#APP_NEXT_PUBLIC_REDASH__URL#$NEXT_PUBLIC_REDASH__URL#g"
+
+exec "$@"
