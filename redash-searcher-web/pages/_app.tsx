7d6103303738f907cba004e4e6bbc6536bf3f33d
Hironori Yamamoto <hironori.yamamoto@gunosy.com>
2023-03-10 12:48:39 +0900

    feat: load eui components

--- redash-searcher-web/pages/_app.tsx
+++ redash-searcher-web/pages/_app.tsx
@@ -1,6 +1,13 @@
 import "instantsearch.css/themes/satellite-min.css";
 import "../styles.css";
+import "@elastic/eui/dist/eui_theme_light.css";
+
+import { EuiProvider } from "@elastic/eui";
 
 export default function App({ Component, pageProps }: any): JSX.Element {
-  return <Component {...pageProps} />;
+  return (
+    <EuiProvider colorMode="light">
+      <Component {...pageProps} />
+    </EuiProvider>
+  );
 }
