c9ac1cff8bf645b6e1ed58234a9f21e5f85e7f19
Hironori Yamamoto <hironori.yamamoto@gunosy.com>
2023-03-23 18:20:49 +0900

    fix: add retrieved_at to openserach fields (#36)

--- redash-searcher-web/pages/_app.tsx
+++ redash-searcher-web/pages/_app.tsx
@@ -1,10 +1,15 @@
 import "instantsearch.css/themes/satellite-min.css";
 import "../styles.css";
 import "@elastic/eui/dist/eui_theme_light.css";
+import { defineCustomElements } from "@duetds/date-picker/dist/loader";
 
 import { EuiProvider } from "@elastic/eui";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: any): JSX.Element {
+  useEffect(() => {
+    defineCustomElements(window);
+  }, []);
   return (
     <EuiProvider colorMode="light">
       <Component {...pageProps} />
